ce062b70597252de582f85e089c6208de56cfcc1
Juanjohn11 <elmatanaco1144@gmail.com>
2022-03-21 18:18:52 -0700

    just pushing what I had

---
+++ JS/quest.js
@@ -0,0 +1,42 @@
+var questions = [{
+    question: "What are variables?",
+    option1: "Containers for storing data values.",
+    option2: "The region of light.",
+    option3: "An action box.",
+    option4: "A special kind of music from Asia.",
+    answer: "1"
+},
+{
+    question: "Is Java and Javascript the same?",
+    option1: "true",
+    option2: "false",
+    option3: " both are true",
+    option4: "both are false.",
+    answer: "2"
+},
+{
+    question: "Which company developed JavaScript?",
+    option1: "Netscape.",
+    option2: "Sony.",
+    option3: "Amazon.",
+    option4: "Apple.",
+    answer: "1"
+},
+{
+    question: "What is a prompt box?",
+    option1: "An area in a theatre in which a prompter sits, located in front of the footlights beneath the stage.",
+    option2: "A music box.",
+    option3: "A popup that has a notice within it.",
+    option4: "A box which allows the user to enter input by providing a text box.",
+    answer: "4"
+},
+{
+    question: "What is a function?",
+    option1: "A block of code designed to perform a particular task.",
+    option2: "An activity that is natural to or the purpose of a person.",
+    option3: " A large or formal social event or ceremony.",
+    option4: "An Array.",
+    answer: "1"
+},
+
+];
